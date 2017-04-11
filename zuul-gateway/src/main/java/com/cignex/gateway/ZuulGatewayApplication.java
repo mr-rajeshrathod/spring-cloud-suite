bb9422d83a3e987056ffe6b2cd39b334fe5a8665
Meet <consultant.meet@gmail.com>
2017-04-11 18:00:54 +0530

    Adding Zuul Proxy server.

---
+++ zuul-gateway/src/main/java/com/cignex/gateway/ZuulGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.cignex.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.cignex.gateway.filter.RequestFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RequestFilter requestFilter() {
+		return new RequestFilter();
+	}
+}
