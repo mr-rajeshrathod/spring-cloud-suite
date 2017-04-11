9a9fbeb84b8974438d47e63aa866906518fc7ec4
Meet <consultant.meet@gmail.com>
2017-04-11 18:10:10 +0530

    Configuration changes in zuul proxy.

--- zuul-gateway/src/main/java/com/cignex/gateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/cignex/gateway/ZuulGatewayApplication.java
@@ -2,11 +2,13 @@ package com.cignex.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import com.cignex.gateway.filter.RequestFilter;
 
+@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulGatewayApplication {
