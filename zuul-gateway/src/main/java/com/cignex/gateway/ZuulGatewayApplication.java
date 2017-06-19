d0d35e811ad887b29c3ed17441b8c1e95b2df71b
Rajesh Rathod <mr.rajeshrathod@gmail.com>
2017-06-19 05:49:58 -0400

    Consul Integration, Removed dependency for Eureka

--- zuul-gateway/src/main/java/com/cignex/gateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/cignex/gateway/ZuulGatewayApplication.java
@@ -2,13 +2,13 @@ package com.cignex.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import com.cignex.gateway.filter.RequestFilter;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulGatewayApplication {
